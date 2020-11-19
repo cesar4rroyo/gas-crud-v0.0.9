aa397ee454bdf26d8fed5835b6c397d114dcdb0f
Cesar <cesardavid_09@hotmail.com>
2020-11-19 11:17:18 -0500

    Reportes parte 4

--- src/java/logic/logicProveedor.java
+++ src/java/logic/logicProveedor.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package logic;
 
 import ModeloDAO.daoProveedor;
 import beans.beanProveedor;
 import java.util.List;
 
-/**
- *
- * @author cesar4rroyo
- */
+
 public class logicProveedor {
     public String insertar(beanProveedor bP) {
         daoProveedor dP = new daoProveedor();
